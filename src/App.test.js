63a9243438d803b86a88cc09d5a310bb729104e5
ismailcanaydin <ismailcanaydin@gmail.com>
2022-10-08 20:31:52 +0300

    Final

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,34 @@
-import {render, screen} from '@testing-library/react;'
-import Header from './Header';
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
 
-describe('Emoji search test', () => {
-  beforeEach()
+describe('app test', () => {
+  beforeEach(() => {
+    render(<App />)
+  });
+
+  test('header', () => {
+    const headerTitle = screen.getByText('Emoji Search');
+    expect(headerTitle).toBeInTheDocument;
+  });
+
+  test('listed', () => {
+    const items = screen.getAllByText('Click to copy emoji');
+    expect(items.length).toEqual(20);
+  });
+
+  test('filter', () => {
+    const emoji = '100';
+    const input = screen.getByRole('textbox');
+    userEvent.type(input, emoji);
+    expect(screen.getByText(emoji)).toBeInTheDocument;
+  });
+
+  test('copy', () => {
+    const click = screen.getAllByText('Click to copy emoji').at(0);
+    const parent = click.parentElement;
+    expect(parent.getAttribute('data-clipboard-text').length).toBeGreaterThan(0);
+  });
 });
